db38641877b9a8600860453b9661e27aa39a7a96
Zen <blueberrysky@gmail.com>
2022-04-19 21:04:29 +0200

    gopoll.py: gps coordinates are not accurate, trying to use remaininKilometers to decide if a new record should be inserted or not

--- gopoll.py
+++ gopoll.py
@@ -13,13 +13,20 @@ db = pymysql.connect(host='localhost',
 x = requests.get('https://greenmo.core.gourban-mobility.com/front/vehicles?lat=52.364431&lng=5.222011&rad=5').json()
 
 for item in x:
-    #turns out remainingKilometers isnt always there
-    if 'remainingKilometers' in item:
-    	print(item['remainingKilometers'])
-    else:
-        item['remainingKilometers'] = 0;
+    #gps is not accurate, compare last record with current data before inserting a new one
     with db.cursor() as cursor:
-        sql = "INSERT IGNORE INTO `go` (`id`, `licensePlate`, `stateOfCharge`, `lat`, `lng`, `remainingKilometers`) VALUES (%s, %s, %s, %s, %s, %s)"
-        cursor.execute(
-        sql, (item['id'], item['licensePlate'], item['stateOfCharge'], item['position']['coordinates'][1], item['position']['coordinates'][0], item['remainingKilometers']))
+        sql = "SELECT `remainingKilometers` FROM `go` WHERE `licensePlate`=%s ORDER BY date DESC LIMIT 1"
+        cursor.execute(sql, (item['licensePlate']))
+        result = cursor.fetchone()
+        if 'remainingKilometers' in item:
+          if ( result != item['remainingKilometers'] ):
+             #turns out remainingKilometers isnt always there
+             if 'remainingKilometers' in item:
+                 print(item['remainingKilometers'])
+             else:
+               item['remainingKilometers'] = 0;
+             with db.cursor() as cursor:
+               sql = "INSERT IGNORE INTO `go` (`id`, `licensePlate`, `stateOfCharge`, `lat`, `lng`, `remainingKilometers`) VALUES (%s, %s, %s, %s, %s, %s)"
+               cursor.execute(
+               sql, (item['id'], item['licensePlate'], item['stateOfCharge'], item['position']['coordinates'][1], item['position']['coordinates'][0], item['remainingKilometers']))
 db.commit()
