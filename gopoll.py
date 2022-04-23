5f978802dcda1f6b1e802cc4abc74ca4a672acec
Zen <blueberrysky@gmail.com>
2022-04-23 19:47:16 +0200

    gopoll.py: using gps percentage change to not store scooter info which are not moving and just depleting battery

--- gopoll.py
+++ gopoll.py
@@ -15,14 +15,17 @@ x = requests.get('https://greenmo.core.gourban-mobility.com/front/vehicles?lat=5
 for item in x:
     #gps is not accurate, compare last record with current data before inserting a new one
     with db.cursor() as cursor:
-        sql = "SELECT `remainingKilometers` FROM `go` WHERE `licensePlate`=%s ORDER BY date DESC LIMIT 1"
+        sql = "SELECT `lng`, `lat`, `remainingKilometers` FROM `go` WHERE `licensePlate`=%s ORDER BY date DESC LIMIT 1"
         cursor.execute(sql, (item['licensePlate']))
         result = cursor.fetchone()
         if 'remainingKilometers' in item:
           if result is not None:
-            if ( result[0] != item['remainingKilometers'] ):
-              with db.cursor() as cursor:
-                 sql = "INSERT IGNORE INTO `go` (`id`, `licensePlate`, `stateOfCharge`, `lat`, `lng`, `remainingKilometers`) VALUES (%s, %s, %s, %s, %s, %s)"
-                 cursor.execute(
-                 sql, (item['id'], item['licensePlate'], item['stateOfCharge'], item['position']['coordinates'][1], item['position']['coordinates'][0], item['remainingKilometers']))
+            change_lng = ((float(result[0])-item['position']['coordinates'][0])/item['position']['coordinates'][0])*100
+            change_lat = ((float(result[1])-item['position']['coordinates'][1])/item['position']['coordinates'][1])*100
+            if change_lng != 0 or change_lat != 0 :
+              if ( result[2] != item['remainingKilometers'] ):
+                with db.cursor() as cursor:
+                   sql = "INSERT IGNORE INTO `go` (`id`, `licensePlate`, `stateOfCharge`, `lat`, `lng`, `remainingKilometers`) VALUES (%s, %s, %s, %s, %s, %s)"
+                   cursor.execute(
+                   sql, (item['id'], item['licensePlate'], item['stateOfCharge'], item['position']['coordinates'][1], item['position']['coordinates'][0], item['remainingKilometers']))
 db.commit()
